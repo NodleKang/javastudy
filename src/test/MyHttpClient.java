f0e64d12dc4b7af7ea89949781d590c08651a060
Nodle Kang <nodlekang@gmail.com>
2023-05-29 18:17:31 +0900

    http client 초기 작성

--- src/test/MyHttpClient.java
+++ src/test/MyHttpClient.java
@@ -4,14 +4,45 @@ import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.HttpClientTransport;
 import org.eclipse.jetty.client.api.ContentResponse;
 import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.client.http.HttpClientTransportOverHTTP;
+import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.client.api.Response;
 import org.eclipse.jetty.client.util.StringContentProvider;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 public class MyHttpClient {
     private final HttpClient httpClient;
 
     public MyHttpClient(int connectionTimeoutMs, int idleTimeoutMs) throws Exception {
-        httpClient = new HttpClient();
+
+        HttpClient httpClientTemp;
+
+        // SSL 컨텍스트 팩토리를 설정하지 않는 경우에는 아래와 같이 설정합니다.
+        // httpClientTemp = new HttpClient();
+
+        // SSL 컨텍스트 팩토리를 설정한 httpClientTemp를 사용하려면 아래와 같이 설정합니다.
+        // SSL 컨텍스트 팩토리는 SSL/TLS 연결을 사용하는 경우에만 설정합니다.
+        httpClientTemp = new HttpClient(new SslContextFactory.Client());
+
+        // 트랜스포트 설정한 httpClientTemp를 사용하려면 아래와 같이 설정합니다.
+        // HTTP 트랜스포트는 HTTP, HTTPS, HTTP/2 등을 설정할 수 있습니다.
+        // HTTP 트랜스포트를 설정하지 않으면 기본값으로 HTTP/1.1이 설정됩니다.
+        // httpClientTemp = new HttpClient(new HttpClientTransportOverHTTP());
+
+        // HttpClient의 스레드 풀을 설정합니다.
+        // 스레드 풀의 크기는 동시에 처리할 수 있는 요청의 개수를 의미합니다.
+        // 스레드 풀의 크기를 설정하지 않으면 기본값으로 200이 설정됩니다.
+        httpClient = httpClientTemp;
+        httpClient.setExecutor(new QueuedThreadPool(100));
+
+        // HttpClient의 트랜스포트를 설정합니다.
+        // 트랜스포트는 HTTP, HTTPS, HTTP/2 등을 설정할 수 있습니다.
+        // 트랜스포트를 설정하지 않으면 기본값으로 HTTP/1.1이 설정됩니다.
+        // httpClient.setTransport(new HttpClientTransportOverHTTP2());
+
+        // 리다아렉트 활성화 여부를 설정합니다. (기본값: true = 활성화)
+        httpClient.setFollowRedirects(true);
 
         // Connection timeout 설정(단위: ms)
         httpClient.setConnectTimeout(connectionTimeoutMs);
@@ -59,4 +90,5 @@ public class MyHttpClient {
     public void stop() throws Exception {
         httpClient.stop();
     }
+
 }
