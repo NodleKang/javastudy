7f027b561618bd80fcf34a591c9eab47b5b03aaf
Nodle Kang <nodlekang@gmail.com>
2023-05-29 18:40:27 +0900

    http client - header, properties 처리 테스트 추가

--- src/test/MyHttpClientTest.java
+++ src/test/MyHttpClientTest.java
@@ -1,34 +1,60 @@
 package test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyHttpClientTest {
     public static void main(String[] args) {
         try {
-            MyHttpClient myHttpClient = new MyHttpClient(5000, 5000);
+            MyHttpClient httpClient = new MyHttpClient(5000, 5000);
 
             // GET 요청 테스트
-            String url = "http://127.0.0.1:3001";
-            String response = myHttpClient.get(url);
-            System.out.println("GET Response:\n" + response);
+            String getUrl = "https://jsonplaceholder.typicode.com/posts/1";
+            String getResponse = httpClient.get(getUrl);
+            System.out.println("GET Response:\n" + getResponse);
 
             // POST 요청 테스트
-            String postUrl = "http://127.0.0.1:3001";
-            String postBody = "param1=value1&param2=value2";
-            String postResponse = myHttpClient.post(postUrl, postBody);
+            String postUrl = "https://jsonplaceholder.typicode.com/posts";
+            String postBody = "{\"title\":\"foo\",\"body\":\"bar\",\"userId\":1}";
+            String postResponse = httpClient.post(postUrl, postBody);
             System.out.println("POST Response:\n" + postResponse);
 
             // PUT 요청 테스트
-            String putUrl = "http://127.0.0.1:3001";
-            String putBody = "updated data";
-            String putResponse = myHttpClient.put(putUrl, putBody);
+            String putUrl = "https://jsonplaceholder.typicode.com/posts/1";
+            String putBody = "{\"id\":1,\"title\":\"foo\",\"body\":\"bar\",\"userId\":1}";
+            String putResponse = httpClient.put(putUrl, putBody);
             System.out.println("PUT Response:\n" + putResponse);
 
             // DELETE 요청 테스트
-            String deleteUrl = "http://127.0.0.1:3001";
-            String deleteResponse = myHttpClient.delete(deleteUrl);
+            String deleteUrl = "https://jsonplaceholder.typicode.com/posts/1";
+            String deleteResponse = httpClient.delete(deleteUrl);
             System.out.println("DELETE Response:\n" + deleteResponse);
 
+            // 요청 헤더 및 속성 설정 테스트를 위한 변수 선언
+            Map<String, String> headers = new HashMap<>();
+            headers.put("Authorization", "Bearer token123");
+            Map<String, String> properties = new HashMap<>();
+            properties.put("param1", "value1");
+            properties.put("param2", "value2");
+
+            // GET 요청 헤더 및 속성 설정 테스트
+            String customGetResponse = httpClient.get(getUrl, headers, properties);
+            System.out.println("Custom GET Response:\n" + customGetResponse);
+
+            // POST 요청 헤더 및 속성 설정 테스트
+            String customPostResponse = httpClient.post(postUrl, postBody, headers, properties);
+            System.out.println("Custom POST Response:\n" + customPostResponse);
+
+            // PUT 요청 헤더 및 속성 설정 테스트
+            String customPutResponse = httpClient.put(putUrl, putBody, headers, properties);
+            System.out.println("Custom PUT Response:\n" + customPutResponse);
+
+            // DELETE 요청 헤더 및 속성 설정 테스트
+            String customDeleteResponse = httpClient.delete(deleteUrl, headers, properties);
+            System.out.println("Custom DELETE Response:\n" + customDeleteResponse);
+
             // HttpClient 종료
-            myHttpClient.stop();
+            httpClient.stop();
         } catch (Exception e) {
             e.printStackTrace();
         }
