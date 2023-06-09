fd7e6b6421f2aae2dc5ba3194ccceb3f462b701c
Nodle Kang <nodlekang@gmail.com>
2023-06-09 15:29:25 +0900

    servlet handler sample

--- src/test/http2/servlet/PathServlet.java
+++ src/test/http2/servlet/PathServlet.java
@@ -37,9 +37,6 @@ public class PathServlet  extends HttpServlet {
         if (uri.startsWith("/path")) {
             PathHandler handler = new PathHandler(req, resp);
             handler.run();
-//            resp.setStatus(200);
-//            resp.setHeader("Content-Type", "application/json");
-//            resp.getWriter().write("{\"result\":\"OK\"}");
         }
     }
 
@@ -50,9 +47,6 @@ public class PathServlet  extends HttpServlet {
         if (uri.startsWith("/path")) {
             PathHandler handler = new PathHandler(req, resp);
             handler.run();
-//            resp.setStatus(200);
-//            resp.setHeader("Content-Type", "application/json");
-//            resp.getWriter().write("{\"result\":\"OK\"}");
         }
     }
 
@@ -63,9 +57,25 @@ public class PathServlet  extends HttpServlet {
         if (uri.startsWith("/path")) {
             PathHandler handler = new PathHandler(req, resp);
             handler.run();
-//            resp.setStatus(200);
-//            resp.setHeader("Content-Type", "application/json");
-//            resp.getWriter().write("{\"result\":\"OK\"}");
         }
     }
+
+    @Override
+    public void destroy() {
+        // 서블릿 종료 시 필요한 작업이 있으면 여기서 처리합니다.
+        // 이 메소드는 서블릿이 종료될 때 한 번만 실행됩니다.
+        // 서블릿이 실행되는 동안 필요한 작업이 없으면 이 메소드는 비워둡니다.
+        System.out.println("destroy");
+    }
+
+    /*
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 서블릿 클래스가 특정 요청 방식(GET, POST 등)에만 관심이 있다면
+        // doGet(), doPost() 등을 오버라이딩해서 작업을 처리하면 됩니다.
+        // 모든 요청 방식에 대해 동일한 작업을 처리하고 싶다면
+        // service() 메소드를 오버라이딩해서 작업을 처리하면 됩니다.
+        System.out.println("service");
+    }
+    */
 }
